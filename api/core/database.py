8d39b3bdb4b16c0e215b87819aae2b39e2565adb
i-see-stars <l.alexander.2.71@gmail.com>
2026-01-09 22:38:36 +0300

    Fix async session

--- api/core/database.py
+++ api/core/database.py
@@ -14,7 +14,4 @@ Base = declarative_base()
 
 async def get_session() -> AsyncSession:
     async with async_session() as session:
-        try:
-            yield session
-        finally:
-            await session.close()
+        yield session
