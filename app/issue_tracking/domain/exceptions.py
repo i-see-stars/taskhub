b7efd29414e75debfa1a57464528cdbc2367836a
i-see-stars <l.alexander.2.71@gmail.com>
2026-03-27 20:12:47 +0700

    feat(issue_tracking): add missing domain exceptions for thin routes refactoring

--- app/issue_tracking/domain/exceptions.py
+++ app/issue_tracking/domain/exceptions.py
@@ -19,3 +19,27 @@ class AssigneeNotProjectMember(Exception):
 
 class IssueParentTypeMismatch(Exception):
     """Raised when parent/child issue type hierarchy is violated."""
+
+
+class DuplicateProjectKey(Exception):
+    """Raised when creating a project with an existing key."""
+
+
+class UserAlreadyProjectMember(Exception):
+    """Raised when adding a user who is already a project member."""
+
+
+class LastOwnerCannotBeRemoved(Exception):
+    """Raised when trying to demote or remove the last project owner."""
+
+
+class MemberNotFound(Exception):
+    """Raised when a project member cannot be located."""
+
+
+class CommentNotFound(Exception):
+    """Raised when a comment cannot be located."""
+
+
+class CommentDeleteNotPermitted(Exception):
+    """Raised when user lacks permission to delete a comment."""
